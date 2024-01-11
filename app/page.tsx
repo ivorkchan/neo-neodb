071b9b5b705210b51a4aa74342b1b512ae95546e
Ivork Chan <github@cy.sb>
2024-01-11 13:19:07 -0500

    Set revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,10 @@ import { Card, CardContent } from '@/components/ui/card'
 
 import categoriesConfig from '@/categoriesConfig.json'
 
-const ItemList = dynamic(() => import('../components/itemlist'), { ssr: false, loading: () => <div className="font-bold text-2xl">LOADING</div> })
+const ItemList = dynamic(() => import('../components/itemlist'), {
+  ssr: false,
+  loading: () => <div className="font-bold text-2xl">LOADING</div>,
+})
 
 export default async function Home() {
   const categoriesData = {}
@@ -16,20 +19,24 @@ export default async function Home() {
   }
 
   return (
-    <main className="min-h-screen flex flex-col gap-16 mx-auto py-24 px-6 lg:max-w-4xl">
+    <main className="min-h-screen flex flex-col gap-24 mx-auto py-36 px-6 lg:max-w-4xl">
       {Object.entries(categoriesData).map(([category, items]) => {
         const defaultTab = Object.keys(categoriesConfig[category])[0]
 
         return (
           <div key={category}>
-            <div className="mb-4 font-bold text-2xl uppercase">{category.charAt(0) + category.slice(1)}</div>
+            <div className="mb-4 font-extrabold text-2xl uppercase">
+              {category.charAt(0) + category.slice(1)}
+            </div>
             <Tabs defaultValue={defaultTab} className="w-full">
               <TabsList>
-                {Object.entries(categoriesConfig[category]).map(([type, title]) => (
-                  <TabsTrigger key={type} value={type}>
-                    {title.toString()}
-                  </TabsTrigger>
-                ))}
+                {Object.entries(categoriesConfig[category]).map(
+                  ([type, title]) => (
+                    <TabsTrigger key={type} value={type}>
+                      {title.toString()}
+                    </TabsTrigger>
+                  ),
+                )}
               </TabsList>
               {Object.entries(categoriesConfig[category]).map(([type]) => (
                 <TabsContent key={type} value={type}>
@@ -37,10 +44,19 @@ export default async function Home() {
                     <CardContent className="pt-6">
                       {category === 'music'
                         ? (
-                          <ItemList items={items[type + category.charAt(0) + category.slice(1)]} imageAspectRatio={1 / 1} />
+                          <ItemList
+                            items={
+                            items[type + category.charAt(0) + category.slice(1)]
+                          }
+                            imageAspectRatio={1 / 1}
+                          />
                           )
                         : (
-                          <ItemList items={items[type + category.charAt(0) + category.slice(1)]} />
+                          <ItemList
+                            items={
+                            items[type + category.charAt(0) + category.slice(1)]
+                          }
+                          />
                           )}
                     </CardContent>
                   </Card>
@@ -61,7 +77,10 @@ async function getItems(category, types) {
 
   for (const type of types) {
     const typeKey = type + category.charAt(0) + category.slice(1)
-    items[typeKey] = await fetchAndMergeData([`${baseApiUrl}${type}?category=${category}&page=1`, `${baseApiUrl}${type}?category=${category}&page=2`])
+    items[typeKey] = await fetchAndMergeData([
+      `${baseApiUrl}${type}?category=${category}&page=1`,
+      `${baseApiUrl}${type}?category=${category}&page=2`,
+    ])
   }
 
   return items
@@ -78,7 +97,9 @@ async function fetchAndMergeData(urls) {
   }
 
   try {
-    const responses = await Promise.all(urls.map(url => fetch(url, options)))
+    const responses = await Promise.all(
+      urls.map(url => fetch(url, options)),
+    )
     const dataArrays = await Promise.all(responses.map(res => res.json()))
     return dataArrays
       .flat()
@@ -89,3 +110,5 @@ async function fetchAndMergeData(urls) {
     console.error('Error fetching data:', error)
   }
 }
+
+export const revalidate = 3600
