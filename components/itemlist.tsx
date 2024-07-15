3baee881831f9fe343fd09ab816e05275a5ecd7f
ivorkchan <github@cy.sb>
2024-07-15 21:13:19 +0800

    Update

--- components/itemlist.tsx
+++ components/itemlist.tsx
@@ -34,64 +34,64 @@ export default function ItemList({ items }) {
     return items.map((item, index) =>
       isDesktop
         ? (
-          <Dialog key={index}>
-            <DialogTrigger>
-              <div className="text-left truncate text-sm leading-6 hover:underline">
-                {item.item.display_title}
-              </div>
-            </DialogTrigger>
-            <DialogContent className="!rounded-none p-8">
-              <DialogHeader>
-                <DialogTitle>{item.item.title}</DialogTitle>
-                <DialogDescription className="truncate-multiline indent-[28px]">
-                  {item.item.brief ? item.item.brief : 'No description.'}
-                </DialogDescription>
-              </DialogHeader>
-              <DialogFooter>
-                <a
-                  href={`https://neodb.social${item.item.url}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Button variant="outline" className="rounded-none shadow-none">
-                    <span className="mr-2">View on NeoDB</span>
-                    <ArrowTopRightIcon />
-                  </Button>
-                </a>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
+            <Dialog key={index}>
+              <DialogTrigger>
+                <div className="text-left truncate text-sm leading-6 hover:underline">
+                  {item.item.display_title}
+                </div>
+              </DialogTrigger>
+              <DialogContent className="!rounded-none p-8">
+                <DialogHeader>
+                  <DialogTitle>{item.item.title}</DialogTitle>
+                  <DialogDescription className="truncate-multiline indent-[28px]">
+                    {item.item.brief ? item.item.brief : 'No description.'}
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                  <a
+                    href={`https://neodb.social${item.item.url}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button variant="outline" className="rounded-none shadow-none">
+                      <span className="mr-2">View on NeoDB</span>
+                      <ArrowTopRightIcon />
+                    </Button>
+                  </a>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           )
         : (
-          <Drawer key={index}>
-            <DrawerTrigger>
-              <div className="text-left truncate text-sm leading-6">
-                {item.item.display_title}
-              </div>
-            </DrawerTrigger>
-            <DrawerContent>
-              <DrawerHeader>
-                <DrawerTitle className="text-left mt-4">
-                  {item.item.title}
-                </DrawerTitle>
-                <DrawerDescription className="truncate-multiline text-left">
-                  {item.item.brief ? item.item.brief : 'No description.'}
-                </DrawerDescription>
-              </DrawerHeader>
-              <DrawerFooter>
-                <a
-                  href={`https://neodb.social${item.item.url}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Button variant="outline" className="rounded-none shadow-none">
-                    <span className="mr-2">View on NeoDB</span>
-                    <ArrowTopRightIcon />
-                  </Button>
-                </a>
-              </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
+            <Drawer key={index}>
+              <DrawerTrigger>
+                <div className="text-left truncate text-sm leading-6">
+                  {item.item.display_title}
+                </div>
+              </DrawerTrigger>
+              <DrawerContent>
+                <DrawerHeader>
+                  <DrawerTitle className="text-left mt-4">
+                    {item.item.title}
+                  </DrawerTitle>
+                  <DrawerDescription className="truncate-multiline text-left">
+                    {item.item.brief ? item.item.brief : 'No description.'}
+                  </DrawerDescription>
+                </DrawerHeader>
+                <DrawerFooter>
+                  <a
+                    href={`https://neodb.social${item.item.url}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button variant="outline" className="rounded-none shadow-none">
+                      <span className="mr-2">View on NeoDB</span>
+                      <ArrowTopRightIcon />
+                    </Button>
+                  </a>
+                </DrawerFooter>
+              </DrawerContent>
+            </Drawer>
           ),
     )
   }
